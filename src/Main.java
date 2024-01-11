d6ecb7f4bd78e00d69512412efd111e603b911b5
MEGACOMP\konst <okonstantinov@easydata.ru>
2024-01-12 01:36:46 +0300

    отформатировал код

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         int ticket = 21456;
-        int bonus = ticket/20;
+        int bonus = ticket / 20;
         System.out.println("бонус будет составлять: " + bonus);
     }
 }
